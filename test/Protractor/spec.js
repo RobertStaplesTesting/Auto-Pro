f28959f599d257f76f24b4e4c2d0979f5a88399e
robstaples <rob.staples91@gmail.com>
2017-01-31 18:25:52 +1100

    Add files via upload

---
+++ test/Protractor/spec.js
@@ -0,0 +1,8 @@
+describe('Protractor Demo App', function() {
+  it('should have a title', function() {
+    browser.get('./index.html');
+    
+
+    expect(browser.getTitle()).toEqual('Super Calculator');
+  });
+});
---
+++ test/spec/test.js
@@ -0,0 +1,9 @@
+(function () {
+  'use strict';
+
+  describe("hello world", function () {
+    it("Says Yes", function () {
+        expect(pros_swt()).toEqual("Yes");
+    });
+  });
+})();
